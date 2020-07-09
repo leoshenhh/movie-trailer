eea79e0129f902c198657c9129ab0da9ed1316df
Shen <343206898@qq.com>
2020-07-09 09:31:41 +0800

    新增html静态文件

---
+++ server/tpl/normal.js
@@ -0,0 +1,26 @@
+module.exports = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width,initial=1">
+      <title>Koa Server HTML</title>
+      <link href="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet">
+      <script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <script src="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.0.0-beta.2/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <h1>Hi shen</h1>
+            <p>This is 申</p>
+          </div>
+          <div class="col-md-4">
+            <p>测试html静态页面</p>
+          </div>
+        </div>
+      </div>
+    </body>
+  </html>
+`

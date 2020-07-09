ea5942b024178c3670af4e5d3c4995774d237834
Shen <343206898@qq.com>
2020-07-09 15:17:56 +0800

    增加模板引擎中间件

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 const Koa = require('koa')
 const app = new Koa()
 const {html,ejsTpl ,pugTpl} = require('./tpl')
+const views = require('koa-views')
+const {resolve} = require('path')
 const ejs = require('ejs')
 const pug = require('pug')
+
+app.use(views(resolve(__dirname,'./views'),{
+  extension: 'pug'
+}))
+
 app.use(async(ctx,next)=>{
-  ctx.type = 'text/html; charset=utf-8'
-  ctx.body = pug.render(pugTpl,{
-    you: 'SHEN',
-    me: '剑指天涯'
+  await ctx.render('index',{
+    you: 'shen',
+    me: '啦啦啦'
   })
 })
+
 app.listen(4455)
